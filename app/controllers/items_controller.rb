432f34def923ae2536970450bd99daac7020ece1
Banyan083214 <gen083214@gmail.com>
2024-03-12 11:58:22 +0900

    ページの遷移設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authorize_item_owner, only: [:edit, :update]
   
   def index
   @items = Item.order(created_at: :desc)
@@ -31,13 +32,20 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path(@item)
     else
       render :edit, status: :unprocessable_entity
     end
   end
   
   private
+
+  def authorize_item_owner
+    @item = Item.find(params[:id])
+    unless current_user && @item.user == current_user
+      redirect_to root_path
+    end
+  end
 
   def item_params
     params.require(:item).permit(
