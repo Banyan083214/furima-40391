e331263a647d54280d0e02515b74577c738f4b23
Banyan083214 <gen083214@gmail.com>
2024-03-12 14:47:31 +0900

    修正1回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :authorize_item_owner, only: [:edit, :update]
+  before_action :authorize_item_owner, only: [:edit, :update, :show]
   
   def index
   @items = Item.order(created_at: :desc)
@@ -22,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
